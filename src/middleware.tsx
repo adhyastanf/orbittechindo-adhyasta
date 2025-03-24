45f3099fa5c568220763de76076a64ec3fee8720
adhyastanf <adhyasta.nf@gmail.com>
2025-03-24 20:46:03 +0700

    fix middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,27 +3,21 @@ import { NextRequest, NextResponse } from 'next/server';
 import { fetchValidationKey } from './lib/services';
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('tmdb_api_key')?.value
+  const token = req.cookies.get('tmdb_api_key')?.value;
 
-  const isAuthRoute = req.nextUrl.pathname.startsWith('/auth');
-
-  if (isAuthRoute) {
-    return NextResponse.next();
-  }
-
-  if (!token) {
-    return NextResponse.redirect(new URL('/auth/login', req.url));
+  if(req.nextUrl.pathname.startsWith('/auth')){
+    return NextResponse.next(); 
   }
 
   try {
-    await fetchValidationKey(token);
+    await fetchValidationKey(token || '');
   } catch {
-    deleteCookie('tmdb_api_key', { req });
     return NextResponse.redirect(new URL('/auth/login', req.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)', '/', '/movie/:type/:id*', '/tv/:type/:id*'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)', '/movie/:type/:id*', '/tv/:type/:id*'],
 };
